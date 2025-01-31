bdbb358233e531130fdbac4666ba4660228eeba7
Frank <frank@osti-online.de>
2025-01-31 11:43:58 +0100

    added description

--- src/main/kotlin/io/github/frantoso/physicalquantities/utils/Utils.kt
+++ src/main/kotlin/io/github/frantoso/physicalquantities/utils/Utils.kt
@@ -3,6 +3,9 @@ package io.github.frantoso.physicalquantities.utils
 import java.text.NumberFormat
 import java.util.Locale
 
+/**
+ * Returns the default number formatter used at several places in the library.
+ */
 private fun createDefaultNumberFormatter(): NumberFormat {
     val formatter = NumberFormat.getNumberInstance(Locale.US)
     formatter.maximumFractionDigits = 6
@@ -10,13 +13,24 @@ private fun createDefaultNumberFormatter(): NumberFormat {
     return formatter
 }
 
+/**
+ * Gets the default number formatter used at several places in the library.
+ * Can be changed using [setDefaultNumberFormatter].
+ * Use [resetDefaultNumberFormatter] to reset it to the library default.
+ */
 var defaultNumberFormatter = createDefaultNumberFormatter()
     private set
 
+/**
+ * Sets the default number formatter to [formatter].
+ */
 fun setDefaultNumberFormatter(formatter: NumberFormat) {
     defaultNumberFormatter = formatter
 }
 
+/**
+ * Resets the default number formatter to the library default.
+ */
 fun resetDefaultNumberFormatter() {
     defaultNumberFormatter = createDefaultNumberFormatter()
 }
